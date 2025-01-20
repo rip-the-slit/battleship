96bdcc55f665c3ddd1568bd3caab585f0e5619d3
rip-the-slit <171191896+rip-the-slit@users.noreply.github.com>
2025-01-20 19:27:58 +0000

    chore: set up eslint jest plugin
    eslint needs this plugin to recognize and validate jest syntax

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,34 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import jestPlugin from 'eslint-plugin-jest';
 
 
 export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      jest: jestPlugin,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...jestPlugin.configs.recommended.rules,
+      ...jestPlugin.configs.style.rules,
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
+    },
+  },
 ];
