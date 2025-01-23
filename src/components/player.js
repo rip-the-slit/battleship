1e61b22acabadd6f4feb3dc1d5194c9345f1b63d
rip-the-slit <171191896+rip-the-slit@users.noreply.github.com>
2025-01-23 03:49:26 +0000

    feat: add player method to automatically populate board

--- src/components/player.js
+++ src/components/player.js
@@ -6,6 +6,33 @@ class Player {
   constructor(gameboard) {
     this.#gameboard = gameboard;
   }
+  autoPlace(ships) {
+    const generateCoordinates = (length) => {
+      const horizontal = Math.random() > 0.5;
+      const coordinates = {
+        startX: Math.ceil(Math.random() * (10 - (horizontal ? length : 0))),
+        startY: Math.ceil(Math.random() * (10 - (!horizontal ? length : 0))),
+      };
+      
+      coordinates["endX"] = coordinates.startX + (horizontal ? length : 0);
+      coordinates["endY"] = coordinates.startY + (!horizontal ? length : 0);
+
+      return coordinates;
+    };
+    ships.forEach((ship) => {
+      let randomPos = generateCoordinates(ship.length);
+      while (
+        !this.#gameboard.place(ship, {
+          startX: randomPos.startX,
+          startY: randomPos.startY,
+          endX: randomPos.endX,
+          endY: randomPos.endY,
+        })
+      ) {
+        randomPos = generateCoordinates(ship.length);
+      }
+    });
+  }
 }
 
 module.exports = Player;
--- test/components/player.test.js
+++ test/components/player.test.js
@@ -1,12 +1,62 @@
-const Player = require("../../src/components/player.js")
-const Gameboard = require("../../src/components/gameboard.js")
-const testPlayer = new Player(new Gameboard())
+const Player = require("../../src/components/player.js");
+const Gameboard = require("../../src/components/gameboard.js");
+let testPlayer;
+beforeEach(() => {
+  testPlayer = new Player(new Gameboard());
+});
 
-test("Exists", () => expect(Player).toBeDefined())
-test("Is a class", () => expect(Object.getPrototypeOf(testPlayer)).toBe(Player.prototype))
-test("Should contain gameboard", () => expect(testPlayer.gameboard).toBeDefined())
+test("Exists", () => expect(Player).toBeDefined());
+test("Is a class", () =>
+  expect(Object.getPrototypeOf(testPlayer)).toBe(Player.prototype));
+test("Should contain gameboard", () =>
+  expect(testPlayer.gameboard).toBeDefined());
 test("Should be able to manipulate gameboard", () => {
-    const testShip = {}
-    testPlayer.gameboard.place(testShip, {startX: 1, startY: 1, endX: 3, endY: 1})
-    expect(testPlayer.gameboard.board[0][1].ship).toBe(testShip)
-})
+  const testShip = {};
+  testPlayer.gameboard.place(testShip, {
+    startX: 1,
+    startY: 1,
+    endX: 3,
+    endY: 1,
+  });
+  expect(testPlayer.gameboard.board[0][1].ship).toBe(testShip);
+});
+describe("Autoplace method", () => {
+  let foundShips;
+  beforeEach(() => {
+    testPlayer.autoPlace([
+      { length: 5 },
+      { length: 4 },
+      { length: 3 },
+      { length: 3 },
+      { length: 2 },
+    ]);
+    foundShips = [];
+  });
+  test("Should populate board randomly", () => {
+    testPlayer.gameboard.board.forEach((row) => {
+      row.forEach((column) => {
+        if (column.ship && !foundShips.includes(column.ship)) {
+          foundShips.push(column.ship);
+        }
+      });
+    });
+    expect(foundShips.length).toBe(5);
+  });
+  test("Autoplaced ships should appear proportionally to their length", () => {
+    testPlayer.gameboard.board.forEach((row) => {
+      row.forEach((column) => {
+        if (column.ship) {
+          const index = foundShips.findIndex((ship) => ship == column.ship);
+          if (index > -1) {
+            if (!foundShips[index]["occurrences"])
+              foundShips[index]["occurrences"] = 1;
+            else foundShips[index].occurrences++;
+          } else foundShips.push(column.ship);
+        }
+      });
+    });
+    foundShips.forEach((ship) => {
+      expect(ship.length == ship.occurrences).toBe(true);
+    });
+  });
+});
